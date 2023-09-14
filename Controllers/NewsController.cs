c1351f481644021d644913da57fdf59e667d3658
Mikhail Shashin <shashinma@icloud.com>
2023-09-14 19:28:32 +0300

    Initial commit

---
+++ Controllers/NewsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using POSTerminalWebApp.Models;
+
+namespace POSTerminalWebApp.Controllers;
+
+public class NewsController : Controller
+{
+    private readonly ILogger<NewsController> _logger;
+
+    public NewsController(ILogger<NewsController> logger)
+    {
+        _logger = logger;
+    }
+    
+    public IActionResult News()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
