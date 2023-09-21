85e320f8e5e7a55dbbca3c170639942f1ea0e16a
Mikhail Shashin <shashinma@icloud.com>
2023-09-21 16:26:03 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using POSTerminalWebApp.Data;
+using POSTerminalWebApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? 
                        throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+// builder.Services.AddDbContext<ApplicationDbContext>(options => 
+//     options.UseSqlite(connectionString));
+
+builder.Services.AddDbContext<ApplicationContext>(options => 
+    options.UseSqlite(connectionString));
+
+// builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+//     .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<ApplicationContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -29,16 +38,22 @@ else
     app.UseHsts();
 }
 
+app.UseDeveloperExceptionPage(); // TODO: only for "dev" branches
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
 
 app.MapControllerRoute(
     "default",
     "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
+
+
+// app.MapRazorPages();
 
 app.Run();
