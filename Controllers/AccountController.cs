903a76772980c75b44ed0817f8aec53828d96ad8
Mikhail Shashin <shashinma@icloud.com>
2023-09-21 16:25:37 +0300

    Create AccountController.cs

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using POSTerminalWebApp.ViewModels;
+using POSTerminalWebApp.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace POSTerminalWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+ 
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                User user = new User { Email = model.Email, UserName = model.Email};
+                // добавляем пользователя
+                var result = await _userManager.CreateAsync(user, model.Password);
+                
+                if (result.Succeeded)
+                {
+                    // установка куки
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+    }
+}
