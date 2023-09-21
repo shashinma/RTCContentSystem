d5b224c328ba07367ec593a4ca1ef6bcad23ac35
Mikhail Shashin <shashinma@icloud.com>
2023-09-21 16:26:09 +0300

    Create RegisterViewModel.cs

---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace POSTerminalWebApp.ViewModels;
+using System.ComponentModel.DataAnnotations;
+
+public class RegisterViewModel
+{
+    [Required]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Пароль")]
+    public string Password { get; set; }
+    
+    [Required]
+    [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Подтвердить пароль")]
+    public string PasswordConfirm { get; set; }
+}
