60b922db6fdf6a7f2847be4017e4d833e0f43604
Mikhail Shashin <shashinma@icloud.com>
2023-09-25 01:26:36 +0300

    upd

--- Program.cs
+++ Program.cs
@@ -5,8 +5,8 @@ using POSTerminalWebApp.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
-                       throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? 
+    throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
