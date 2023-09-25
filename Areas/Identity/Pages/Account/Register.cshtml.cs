3361e9704a3277f05341034fa9d54abb1c87b419
Mikhail Shashin <shashinma@icloud.com>
2023-09-25 20:17:18 +0300

    upd

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -153,6 +153,7 @@ namespace POSTerminalWebApp.Areas.Identity.Pages.Account
                     ModelState.AddModelError(string.Empty, error.Description);
                 }
             }
+            
             // If we got this far, something failed, redisplay form
             return Page();
         }
