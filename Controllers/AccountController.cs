e135e3e04a296452fd7c1b13cb3c7da849624d25
Mikhail Shashin <shashinma@icloud.com>
2023-09-25 14:05:25 +0300

    Init AccountController.cs

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using POSTerminalWebApp.Models;
+
+namespace POSTerminalWebApp.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+
+    public AccountController(ILogger<AccountController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Auth()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
