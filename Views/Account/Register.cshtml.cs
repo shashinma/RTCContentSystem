d5ee9f751216af04dbb47731e5e6984fc2d3502e
Mikhail Shashin <shashinma@icloud.com>
2023-09-26 17:57:31 +0300

    upd

---
+++ Views/Account/Register.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace POSTerminalWebApp.Views.Account;
+
+public class Register : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
