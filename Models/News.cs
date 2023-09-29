4c60e90b52bf16f94858545cfdc397e07450bc91
Mikhail Shashin <shashinma@icloud.com>
2023-09-29 17:36:05 +0300

    Add CRUD

---
+++ Models/News.cs
@@ -0,0 +1,11 @@
+﻿namespace POSTerminalWebApp.Models;
+
+public class News
+{
+    public int ID { get; set; }
+    public string Title {get; set; }
+    public string Description {get; set; }
+    public string Author {get; set; }
+    public DateTime Created {get; set; }
+    public DateTime Published {get; set; }
+}
