5efcfdb2717248106d205021a95235a2aa648a14
Mikhail Shashin <shashinma@icloud.com>
2023-10-10 13:47:26 +0300

    update

--- Models/News.cs
+++ Models/News.cs
@@ -3,9 +3,13 @@
 public class News
 {
     public int ID { get; set; }
+    
     public string Title { get; set; }
     public string Description { get; set; }
-    public string? Author { get; set; }
+    
+    public string Author { get; set; }
+    public string Publisher { get; set; }
+    
     public DateTime Created { get; set; }
     public DateTime Published { get; set; }
 }
