403025cb0e0d9c3f2e70a0cd4e5a79f57f4e3eb6
Mikhail Shashin <shashinma@icloud.com>
2023-10-13 08:12:50 +0300

    add keyboard

--- Controllers/NewsController.cs
+++ Controllers/NewsController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using POSTerminalWebApp.Data;
 using POSTerminalWebApp.Models;
@@ -15,6 +16,7 @@ public class NewsController : Controller
         _logger = logger;
     }
     
+    [Authorize]
     public IActionResult News()
     {
         return View();
