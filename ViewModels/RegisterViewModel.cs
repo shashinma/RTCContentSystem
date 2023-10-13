929ab1d521409462b3eb30006f2f5e90b36ff250
Mikhail Shashin <shashinma@icloud.com>
2023-10-13 12:27:09 +0300

    Init

---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace POSTerminalWebApp.ViewModels;
+using System.ComponentModel.DataAnnotations;
+
+public class RegisterViewModel
+{
+    [Required]
+    [DataType(DataType.EmailAddress)]
+    [RegularExpression ("[a-z0-9._%+-]+@rtc.ru")]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Пароль")]
+    public string Password { get; set; }
+    
+    [Required]
+    [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Подтвердить пароль")]
+    public string PasswordConfirm { get; set; }
+}
