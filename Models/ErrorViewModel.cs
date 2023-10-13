a593365fea2de43500c6d8c3badceeef7c850577
Mikhail Shashin <shashinma@icloud.com>
2023-10-13 12:27:09 +0300

    Init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace POSTerminalWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
