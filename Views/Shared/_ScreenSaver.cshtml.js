d5da8300681522621a68cf5e79920006c91c3400
Mikhail Shashin <shashinma@icloud.com>
2023-10-19 21:47:15 +0300

    upd

---
+++ Views/Shared/_ScreenSaver.cshtml.js
@@ -0,0 +1,22 @@
+var timeout;
+var popup = document.getElementById('screen-saver');
+var minutes = 5;
+
+function resetTimer() {
+    clearTimeout(timeout);
+    timeout = setTimeout(showPopup, minutes * 60 * 1000); // 5 minutes
+}
+
+function showPopup() {
+    popup.style.display = 'block';
+}
+
+function hidePopup() {
+    popup.style.display = 'none';
+    resetTimer(); // Start the timer again after hiding the popup
+}
+
+popup.addEventListener('click', hidePopup);
+document.addEventListener('mousemove', resetTimer);
+
+resetTimer(); // Start the timer initially
