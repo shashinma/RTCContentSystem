e582d2f7373086873fb2d47f8e96feaed5cb3025
Mikhail Shashin <shashinma@icloud.com>
2023-10-20 13:14:50 +0300

    Get localstorage for layout

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using System.DirectoryServices.Protocols;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using POSTerminalWebApp.Controllers;
 using POSTerminalWebApp.Services;
 using POSTerminalWebApp.Data;
 
--- Views/Shared/_Layout.cshtml.js
+++ Views/Shared/_Layout.cshtml.js
@@ -5,10 +5,30 @@ for (var i = 0; i < arrow.length; i++) {
         arrowParent.classList.toggle("showMenu");
     });
 }
+
 let sidebar = document.querySelector(".sidebar");
 let sidebarBtn = document.querySelector(".bx-menu");
 console.log(sidebarBtn);
+
+// Проверяем, есть ли сохраненное состояние в localStorage или sessionStorage
+if (localStorage.getItem("sidebarState") === "closed") {
+    sidebar.classList.add("close");
+}
+
 sidebarBtn.addEventListener("click", () => {
     sidebar.classList.toggle("close");
+
+    // Сохраняем состояние в localStorage или sessionStorage
+    if (sidebar.classList.contains("close")) {
+        localStorage.setItem("sidebarState", "closed");
+    } else {
+        localStorage.setItem("sidebarState", "open");
+    }
 });
 
+var sidebarState = localStorage.getItem('sidebarState');
+console.log(sidebarState);
+
+
+// let value = localStorage.getItem("sidebarState");
+// let sidebarClass = value === null || value === "" ? "sidebar open" : value === "open" ? "sidebar open" : "sidebar";
