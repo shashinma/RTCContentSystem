716f9b7e15696412da1b0a45d893baa8a77ffe39
Mikhail Shashin <shashinma@icloud.com>
2023-10-20 13:14:50 +0300

    Get localstorage for layout

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using POSTerminalWebApp.Controllers;
 using POSTerminalWebApp.Services;
 using POSTerminalWebApp.Data;
 
