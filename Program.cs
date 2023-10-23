5aab3f3a9cbba9aeadbe3798f9b7b2aaf36f5e66
Mikhail Shashin <shashinma@icloud.com>
2023-10-23 18:01:29 +0300

    ~

--- Program.cs
+++ Program.cs
@@ -29,13 +29,6 @@ builder.Services.AddScoped<IMenuService, MenuService>();
 builder.Services.AddScoped<INewsService, NewsService>();
 builder.Services.AddScoped<IJobsService, JobsService>();
 
-builder.Services.AddHttpsRedirection(options =>
-{
-    options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
-    options.HttpsPort = 443;
-    
-});
-
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
