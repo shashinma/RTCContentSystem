95f4af8340c39d8d777d55a86a8317bd61cfdf0f
Mikhail Shashin <shashinma@icloud.com>
2023-10-24 00:00:20 +0300

    Update News

---
+++ Views/News/News.cshtml.cs
@@ -0,0 +1,27 @@
+﻿namespace POSTerminalWebApp.Views.News;
+
+public class NewsAction
+{
+    public static string GetLimitedWords(string input, int wordCount)
+    {
+        string[] words = input.Split(' ');
+        if (words.Length <= wordCount)
+        {
+            return input;
+        }
+        else
+        {
+            string limitedContent = string.Join(' ', words.Take(wordCount));
+            limitedContent = TrimPunctuation(limitedContent);
+            limitedContent += "...";
+            return limitedContent;
+        }
+    }
+
+    public static string TrimPunctuation(string input)
+    {
+        char[] punctuation = { '.', ',', '!', '?' }; // Добавьте другие знаки препинания по необходимости
+        input = input.TrimEnd(punctuation);
+        return input;
+    }
+}
