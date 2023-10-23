6035962bc3a82500be43bb8c5482c77e330b7587
Mikhail Shashin <shashinma@icloud.com>
2023-10-23 16:27:44 +0300

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,7 @@
 ﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using POSTerminalWebApp.Models;
-using Microsoft.AspNetCore.Authorization;
-
 
 namespace POSTerminalWebApp.Controllers;
 
