de9b1f39717b85916e2738e9e555fd938d4db232
Mikhail Shashin <shashinma@icloud.com>
2023-10-25 20:08:35 +0300

    delete title

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -7,9 +7,7 @@ public class ApplicationDbContext : DbContext
 {
     public DbSet<MenuItem> MenuItems { get; set; }
     public DbSet<SubMenuItem> SubMenuItems { get; set; }
-    
     public DbSet<NewsItem> NewsItems { get; set; }
-    
     public DbSet<JobsItem> JobsItems { get; set; }
     
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
