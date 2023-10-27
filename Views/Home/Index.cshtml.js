5c6d498da2da807a76711fa5f6d6c9cef059a028
Mikhail Shashin <shashinma@icloud.com>
2023-10-27 06:56:58 +0300

    Edit Home

---
+++ Views/Home/Index.cshtml.js
@@ -0,0 +1,11 @@
+$(".custom-carousel").owlCarousel({
+    autoWidth: true,
+    loop: true
+});
+$(document).ready(function () {
+    $(".custom-carousel .item").click(function () {
+        $(".custom-carousel .item").not($(this)).removeClass("active");
+        $(this).toggleClass("active");
+    });
+});
+
