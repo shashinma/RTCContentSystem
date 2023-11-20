069ca4961d64c1f91d1c49cc456ec54fd7bfaace
Mikhail Shashin <shashinma@icloud.com>
2023-11-20 17:01:54 +0300

    Dev (#225)
    * Create global.json
    
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>
    
    * Update app.db
    
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>
    
    * Update appsettings.json
    
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>
    
    * Update identity.db
    
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>
    
    * Update NewsController.cs
    
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>
    
    * Update NewsItemModel.cs
    
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>
    
    * Update Index.cshtml.js
    
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>
    
    * Update Create.cshtml
    
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>
    
    * Delete Edit.cshtml
    
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>
    
    * Update Index.cshtml.css
    
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>
    
    * Update Index.cshtml
    
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>
    
    * Update _Layout.cshtml.js
    
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>
    
    * Update _MenuPartial.cshtml
    
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>
    
    * Update _NavBar.cshtml
    
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>
    
    * Update _NavBar.cshtml.css
    
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: akinin <3820354+akinin@users.noreply.github.com>

--- POSTerminal/Controllers/NewsController.cs
+++ POSTerminal/Controllers/NewsController.cs
@@ -140,4 +140,87 @@ public class NewsController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+
+    // // GET: NewsControllerAlt/Details/5
+    // public ActionResult Details(int id)
+    // {
+    //     return View();
+    // }
+    //
+    
+    public ActionResult Create()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Create(NewsItem model)
+    {
+            try
+            {
+                var newsItem = new NewsItem
+                {
+                    Title = model.Title,
+                    Content = model.Content,
+                    PicSrc = model.PicSrc,
+                    PublishDate = DateTime.Now
+                };
+                _context.NewsItems.Add(newsItem);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return RedirectToAction("Index");
+        }
+    
+    //
+    // // GET: NewsControllerAlt/Edit/5
+    // public ActionResult Edit(int id)
+    // {
+    //     return View();
+    // }
+    //
+    // // POST: NewsControllerAlt/Edit/5
+    // [HttpPost]
+    // [ValidateAntiForgeryToken]
+    // public ActionResult Edit(int id, IFormCollection collection)
+    // {
+    //     try
+    //     {
+    //         // TODO: Add update logic here
+    //
+    //         return RedirectToAction(nameof(Index));
+    //     }
+    //     catch
+    //     {
+    //         return View();
+    //     }
+    // }
+    //
+    // // GET: NewsControllerAlt/Delete/5
+    // public ActionResult Delete(int id)
+    // {
+    //     return View();
+    // }
+    //
+    // // POST: NewsControllerAlt/Delete/5
+    // [HttpPost]
+    // [ValidateAntiForgeryToken]
+    // public ActionResult Delete(int id, IFormCollection collection)
+    // {
+    //     try
+    //     {
+    //         // TODO: Add delete logic here
+    //
+    //         return RedirectToAction(nameof(Index));
+    //     }
+    //     catch
+    //     {
+    //         return View();
+    //     }
+    // }
 }
