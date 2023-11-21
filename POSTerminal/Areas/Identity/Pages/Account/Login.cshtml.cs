890899a0476d5c72c518830eccb36e58f9d7e767
Mikhail Shashin <shashinma@icloud.com>
2023-11-21 17:51:30 +0300

    Update Login.cshtml.cs
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>

--- POSTerminal/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ POSTerminal/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace AuthSystem.Areas.Identity.Pages.Account;
+namespace POSTerminal.Areas.Identity.Pages.Account;
 
 public class LoginModel : PageModel
 {
