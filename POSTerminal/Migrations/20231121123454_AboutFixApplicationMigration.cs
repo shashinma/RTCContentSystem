e3058a2ade66f5ca3dd7f16009ba49390bc1a566
Mikhail Shashin <shashinma@icloud.com>
2023-11-21 17:52:50 +0300

    Create 20231121123454_AboutFixApplicationMigration.cs
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>

---
+++ POSTerminal/Migrations/20231121123454_AboutFixApplicationMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace POSTerminal.Migrations
+{
+    /// <inheritdoc />
+    public partial class AboutFixApplicationMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "AboutItems");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "AboutItems",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
