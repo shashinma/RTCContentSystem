cc3f8af4b2c7fe2587cc123aae34ffacb8e4b5e6
Mikhail Shashin <shashinma@icloud.com>
2023-11-21 12:19:44 +0300

    Update MuseumController.cs
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>

--- POSTerminal/Controllers/MuseumController.cs
+++ POSTerminal/Controllers/MuseumController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using POSTerminal.Data;
 using POSTerminal.Models;
 
 namespace POSTerminal.Controllers;
@@ -8,16 +9,26 @@ namespace POSTerminal.Controllers;
 [Authorize]
 public class MuseumController : Controller
 {
+    private readonly ApplicationDbContext _context;
     private readonly ILogger<MuseumController> _logger;
-
-    public MuseumController(ILogger<MuseumController> logger)
+    
+    public MuseumController(ApplicationDbContext context, ILogger<MuseumController> logger)
     {
+        _context = context;
         _logger = logger;
     }
 
     public IActionResult Index()
     {
         return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Create(MuseumItem model)
+    {
+        _context.MuseumItems.Add(model);
+        _context.SaveChanges();
+        return RedirectToAction("Index", "Home");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
