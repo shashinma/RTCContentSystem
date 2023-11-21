0c170ca4fad1187ada552f737c7cc1d3e0cf8fe3
Mikhail Shashin <shashinma@icloud.com>
2023-11-21 17:51:48 +0300

    Update ChangePassword.cshtml.cs
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>

--- POSTerminal/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ POSTerminal/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace POSTerminalWebApp.Areas.Identity.Pages.Account.Manage;
+namespace POSTerminal.Areas.Identity.Pages.Account.Manage;
 
 public class ChangePasswordModel : PageModel
 {
