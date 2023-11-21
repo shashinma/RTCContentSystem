b2a511c1d131b859b833903a8df932fbae064837
Mikhail Shashin <shashinma@icloud.com>
2023-11-21 17:52:39 +0300

    Update RegisterConfirmation.cshtml.cs
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>

--- POSTerminal/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ POSTerminal/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 
-namespace POSTerminalWebApp.Areas.Identity.Pages.Account;
+namespace POSTerminal.Areas.Identity.Pages.Account;
 
 [AllowAnonymous]
 public class RegisterConfirmationModel : PageModel
