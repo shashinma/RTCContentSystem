bddc357bf3ff058e7dd7eb7d9db6d8dc8858b6b7
Mikhail Shashin <shashinma@icloud.com>
2024-01-17 21:26:12 +0300

    Implement IHomeService interface in HomeService
    The HomeService class has been modified to implement the IHomeService interface. It now contains a GetNews method, which retrieves news items from the application's context. New dependencies for EntityFrameworkCore, POSTerminal's Data and Models namespaces have been also added.

--- POSTerminal/Services/HomeService.cs
+++ POSTerminal/Services/HomeService.cs
@@ -1,5 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using POSTerminal.Data;
+using POSTerminal.Models;
+
 namespace POSTerminal.Services;
 
-public class HomeService
+public interface IHomeService
 {
+    IEnumerable<NewsItem> GetNews();
+}
+
+public class HomeService : IHomeService
+{
+    private readonly ApplicationDbContext _context;
+
+    public HomeService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<NewsItem> GetNews()
+    {
+        return _context.NewsItems.Include(n => n.Image).ToList();
+    }
 }
