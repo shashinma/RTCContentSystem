80903c491908207d14053d7d39206de528eb71f9
Mikhail Shashin <shashinma@icloud.com>
2024-01-17 21:27:07 +0300

    Add Image properties to NewsItemModel
    Two new properties, ImageId and Image, have been added to the NewsItemModel class. This change will make it possible to associate an image with each news item in the POS terminal.

--- POSTerminal/Models/NewsItemModel.cs
+++ POSTerminal/Models/NewsItemModel.cs
@@ -11,5 +11,8 @@ public class NewsItem
     public string? Author { get; set; }
     public string? Tags { get; set; }
     public string? TimeToRead { get; set; }
+    public int? ImageId { get; set; }
+    public ImageModel? Image { get; set; }
     public DateTime PublishDate { get; set; }
+
 }
