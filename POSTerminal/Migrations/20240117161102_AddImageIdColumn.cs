0928b31469389b733bd3286d4f7b6ce6fc6e3836
Mikhail Shashin <shashinma@icloud.com>
2024-01-17 21:27:24 +0300

    Add ImageId column to NewsItems table
    This commit introduces a new nullable ImageId column to the NewsItems table in the POSTerminal application, in order to keep a reference to related images. It also includes the creation of the ImageItems table to store all images, along with the necessary migrational changes. These updates will improve the management and retrieval of image data associated with NewsItems.

---
+++ POSTerminal/Migrations/20240117161102_AddImageIdColumn.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace POSTerminal.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageIdColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ImageId",
+                table: "NewsItems",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ImageItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Image = table.Column<byte[]>(type: "BLOB", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ImageItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NewsItems_ImageId",
+                table: "NewsItems",
+                column: "ImageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_NewsItems_ImageItems_ImageId",
+                table: "NewsItems",
+                column: "ImageId",
+                principalTable: "ImageItems",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_NewsItems_ImageItems_ImageId",
+                table: "NewsItems");
+
+            migrationBuilder.DropTable(
+                name: "ImageItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_NewsItems_ImageId",
+                table: "NewsItems");
+
+            migrationBuilder.DropColumn(
+                name: "ImageId",
+                table: "NewsItems");
+        }
+    }
+}
