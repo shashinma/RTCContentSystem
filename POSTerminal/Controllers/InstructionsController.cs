7e0e4f80191ff3ba246f14f5d80f8da3450a62bc
Mikhail Shashin <shashinma@icloud.com>
2024-01-30 15:02:05 +0300

    Update InstructionController for image and document handling
    The InstructionsController has been enhanced to handle image and document files. The update and create methods now support asynchronous operations, and they can accept, process, and store image and document data. Unnecessary methods were also commented out.

--- POSTerminal/Controllers/InstructionsController.cs
+++ POSTerminal/Controllers/InstructionsController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using POSTerminal.Data;
 using POSTerminal.Models;
+using POSTerminal.Services;
 
 namespace POSTerminal.Controllers;
 
@@ -12,10 +14,13 @@ public class InstructionsController : Controller
     private readonly ApplicationDbContext _context;
     private readonly ILogger<InstructionsController> _logger;
     
-    public InstructionsController(ApplicationDbContext context, ILogger<InstructionsController> logger)
+    private readonly IImageService _imageService;
+    
+    public InstructionsController(ApplicationDbContext context, ILogger<InstructionsController> logger, IImageService imageService)
     {
         _context = context;
         _logger = logger;
+        _imageService = imageService;
     }
 
     public IActionResult Index()
@@ -24,12 +29,87 @@ public class InstructionsController : Controller
     }
     
     [HttpPost]
-    public IActionResult Create(InstructionItem model)
+    public async Task<IActionResult> Create(InstructionItem model, IFormFile image, IFormFile document)
     {
-        _context.InstructionItems.Add(model);
-        _context.SaveChanges();
-        return RedirectToAction("Index");
+        ImageModel? imageModel = null;
+        DocumentModel? documentModel = null;
+
+        if (image != null)
+        {
+            byte[] imageData = null;
+
+            // считываем переданный файл в массив байт
+            using (var binaryReader = new BinaryReader(image.OpenReadStream()))
+            {
+                imageData = binaryReader.ReadBytes((int)image.Length);
+            }
+
+            imageModel = new ImageModel 
+            {
+                Image = imageData,
+                Name = image.FileName
+            };
+
+            _context.ImageItems.Add(imageModel);
+            await _context.SaveChangesAsync();
+        }
+        else if (!string.IsNullOrEmpty(model.ImageUrl))
+        {
+            // If image file was not provided, but URL for image (PicSrc) is present, use the ImageService to download and save the image
+            var result = await _imageService.DownloadAndSaveImage(model.ImageUrl);
+
+            if (result)
+            {
+                // If image is downloaded and saved successfuly, retrieve the ImageModel that we just saved
+                imageModel = await _context.ImageItems.FirstOrDefaultAsync(i => i.Name == Path.GetFileName(model.ImageUrl));
+            }
+        }
+        
+        if (document != null)
+        {
+            byte[] documentData = null;
+
+            // считываем переданный файл в массив байт
+            using (var binaryReader = new BinaryReader(document.OpenReadStream()))
+            {
+                documentData = binaryReader.ReadBytes((int)document.Length);
+            }
+
+            documentModel = new DocumentModel() 
+            {
+                Document = documentData,
+                Name = document.FileName
+            };
+
+            _context.DocumentItems.Add(documentModel);
+            await _context.SaveChangesAsync();
+        }
+        
+        var instruction = new InstructionItem()
+        {
+            Title = model.Title,
+            ImageUrl = model.ImageUrl,
+        };
+    
+        if (imageModel != null)
+        {
+            instruction.ImageId = imageModel.Id;
+            instruction.Image = imageModel;
+        }
+
+        if (documentModel != null)
+        {
+            instruction.DocumentId = documentModel.Id;
+            instruction.Document = documentModel;
+            instruction.LinkToPage = Path.GetFileNameWithoutExtension(documentModel.Name);
+        }
+
+        _context.InstructionItems.Add(instruction);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
     }
+
     
     public JsonResult Delete(int id)
     {
@@ -46,24 +126,106 @@ public class InstructionsController : Controller
         return Json(result);
     }
     
-    public IActionResult GetInstructions(int id)
+        [HttpPost]
+    public async Task<IActionResult> Update(InstructionItem model, IFormFile image, IFormFile document)
     {
-        var news = _context.InstructionItems.Find(id);
-        return Json(news);
+        ImageModel? imageModel = null;
+        DocumentModel? documentModel = null;
+
+        if (image != null)
+        {
+            byte[] imageData = null;
+
+            // считываем переданный файл в массив байт
+            using (var binaryReader = new BinaryReader(image.OpenReadStream()))
+            {
+                imageData = binaryReader.ReadBytes((int)image.Length);
+            }
+
+            imageModel = new ImageModel 
+            {
+                Image = imageData,
+                Name = image.FileName
+            };
+
+            _context.ImageItems.Add(imageModel);
+            await _context.SaveChangesAsync();
+        }
+        else if (!string.IsNullOrEmpty(model.ImageUrl))
+        {
+            // If image file was not provided, but URL for image (PicSrc) is present, use the ImageService to download and save the image
+            var result = await _imageService.DownloadAndSaveImage(model.ImageUrl);
+
+            if (result)
+            {
+                // If image is downloaded and saved successfuly, retrieve the ImageModel that we just saved
+                imageModel = await _context.ImageItems.FirstOrDefaultAsync(i => i.Name == Path.GetFileName(model.ImageUrl));
+            }
+        }
+        
+        if (document != null)
+        {
+            byte[] documentData = null;
+
+            // считываем переданный файл в массив байт
+            using (var binaryReader = new BinaryReader(document.OpenReadStream()))
+            {
+                documentData = binaryReader.ReadBytes((int)document.Length);
+            }
+
+            documentModel = new DocumentModel() 
+            {
+                Document = documentData,
+                Name = document.FileName
+            };
+
+            _context.DocumentItems.Add(documentModel);
+            await _context.SaveChangesAsync();
+        }
+        
+        var instruction = new InstructionItem()
+        {
+            Title = model.Title,
+            ImageUrl = model.ImageUrl,
+        };
+    
+        if (imageModel != null)
+        {
+            instruction.ImageId = imageModel.Id;
+            instruction.Image = imageModel;
+        }
+
+        if (documentModel != null)
+        {
+            instruction.DocumentId = documentModel.Id;
+            instruction.Document = documentModel;
+            instruction.LinkToPage = Path.GetFileNameWithoutExtension(documentModel.Name);
+        }
+
+        _context.InstructionItems.Add(instruction);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
     }
     
-    [HttpPost]
-    public IActionResult Update(InstructionItem model)
-    {
-        var InstructionFromDb = _context.InstructionItems.Find(model.Id);
-        InstructionFromDb.Title = model.Title;
-        InstructionFromDb.ImageUrl = model.ImageUrl;
-        
-        _context.InstructionItems.Update(InstructionFromDb);
-        _context.SaveChanges();
-        
-        return RedirectToAction("Index");
-    }
+    // public IActionResult GetInstructions(int id)
+    // {
+    //     var news = _context.InstructionItems.Find(id);
+    //     return Json(news);
+    // }
+    
+    // [HttpPost]
+    // public IActionResult Update(InstructionItem model)
+    // {
+    //     var InstructionFromDb = _context.InstructionItems.Find(model.Id);
+    //     InstructionFromDb.Title = model.Title;
+    //     InstructionFromDb.ImageUrl = model.ImageUrl;
+    //     
+    //     _context.InstructionItems.Update(InstructionFromDb);
+    //     _context.SaveChanges();
+    //     
+    //     return RedirectToAction("Index");
+    // }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
