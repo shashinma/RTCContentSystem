2e42dc9fb4005173757a9780611ff0bcbc01bb6a
Mikhail Shashin <shashinma@icloud.com>
2024-01-30 15:03:20 +0300

    Add properties to ViewerItemModel
    A few properties are added to the ViewerItemModel for better representation. Specifically, the 'Id' and 'DocumentId' properties are added and both are of integer type. The 'Document' property is also added and is of the 'DocumentModel' type.

--- POSTerminal/Models/ViewerItemModel.cs
+++ POSTerminal/Models/ViewerItemModel.cs
@@ -2,5 +2,11 @@ namespace POSTerminal.Models;
 
 public class ViewerItem
 {
+    public int Id { get; set; }
+
     public string? name { get; set; }
+    
+    public int? DocumentId { get; set; }
+    
+    public DocumentModel? Document { get; set; }
 }
