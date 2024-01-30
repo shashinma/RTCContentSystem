72d3eb870c99a0231d912f702159a7aec4a1748c
Mikhail Shashin <shashinma@icloud.com>
2024-01-30 15:03:08 +0300

    Add optional properties to InstructionsItemModel
    Nullable properties for ImageUrl, ImageId, ImageModel, DocumentId, and DocumentModel have been added to the InstructionsItemModel. This will facilitate handling instances where these properties may not have values, thereby providing improved flexibility and error resistance.

--- POSTerminal/Models/InstructionsItemModel.cs
+++ POSTerminal/Models/InstructionsItemModel.cs
@@ -4,6 +4,10 @@ public class InstructionItem
 {
     public int Id { get; set; }
     public string Title { get; set; }
-    public string ImageUrl { get; set; }
+    public string? ImageUrl { get; set; }
+    public int? ImageId { get; set; }
+    public ImageModel? Image { get; set; }
+    public int? DocumentId { get; set; }
+    public DocumentModel? Document { get; set; }
     public string? LinkToPage { get; set; }
 }
