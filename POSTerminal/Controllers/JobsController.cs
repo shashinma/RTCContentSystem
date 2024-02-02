22cdac7dc42a7e1149f8adeaff48836e398bbd02
Mikhail Shashin <shashinma@icloud.com>
2024-02-02 14:29:38 +0300

    Add image handling for job updates
    Enhancements were made to the JobsController, adding support for handling images during job updates. An asynchronous Update method was created which incorporates image upload or external image source linking. The previous Update method was commented out. An IImageService instance was also introduced for potential usage.

--- POSTerminal/Controllers/JobsController.cs
+++ POSTerminal/Controllers/JobsController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using POSTerminal.Data;
 using POSTerminal.Models;
+using POSTerminal.Services;
 
 namespace POSTerminal.Controllers;
 
@@ -11,6 +13,8 @@ public class JobsController : Controller
 {
     private readonly ApplicationDbContext _context;
     private readonly ILogger<JobsController> _logger;
+    
+    private readonly IImageService _imageService;
     
     public JobsController(ApplicationDbContext context, ILogger<JobsController> logger)
     {
@@ -23,13 +27,65 @@ public class JobsController : Controller
         return View();
     }
     
+    // TODO: Проверка на NULL
     [HttpPost]
-    public IActionResult Create(JobsItem model)
+    public IActionResult Create(JobsItem model, IFormFile image)
     {
         _context.JobsItems.Add(model);
         _context.SaveChanges();
         return RedirectToAction("Index");
     }
+    
+    [HttpPost]
+    public async Task<IActionResult> Update(int id, JobsItem model, IFormFile image)
+    {
+        ImageModel? imageModel = null;
+        var jobs = await _context.JobsItems.FindAsync(id);
+
+        if (image != null)
+        {
+            byte[] imageData = null;
+
+            using (var binaryReader = new BinaryReader(image.OpenReadStream()))
+            {
+                imageData = binaryReader.ReadBytes((int)image.Length);
+            }
+
+            imageModel = new ImageModel 
+            {
+                Image = imageData,
+                Name = image.FileName
+            };
+
+            _context.ImageItems.Add(imageModel);
+            await _context.SaveChangesAsync();
+        
+            // jobs.ImageId = imageModel.Id;
+            // jobs.Image = imageModel;
+            jobs.PicSrc = null;
+        }
+        else if (!string.IsNullOrEmpty(model.PicSrc))
+        {
+            var result = await _imageService.DownloadAndSaveImage(model.PicSrc);
+    
+            if (result)
+            {
+                imageModel = await _context.ImageItems.FirstOrDefaultAsync(i => i.Name == Path.GetFileName(model.PicSrc));
+                
+                // jobs.ImageId = imageModel.Id;
+                // jobs.Image = imageModel;
+                jobs.PicSrc = model.PicSrc;
+            }
+        }
+
+        jobs.Vacancy = model.Vacancy;
+        jobs.Responsibilities = model.Responsibilities;
+        jobs.Requirements = model.Requirements;
+
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
+    }   
     
     public JsonResult Delete(int id)
     {
@@ -52,20 +108,21 @@ public class JobsController : Controller
         return Json(jobs);
     }
     
-    [HttpPost]
-    public IActionResult Update(JobsItem model)
-    {
-        var jobsFromDb = _context.JobsItems.Find(model.Id);
-        jobsFromDb.Vacancy = model.Vacancy;
-        jobsFromDb.Requirements = model.Requirements;
-        jobsFromDb.Responsibilities = model.Responsibilities;
-        jobsFromDb.PicSrc = model.PicSrc;
-        
-        _context.JobsItems.Update(jobsFromDb);
-        _context.SaveChanges();
-        
-        return RedirectToAction("Index");
-    }
+    
+    
+    // public IActionResult Update(JobsItem model)
+    // {
+    //     var jobsFromDb = _context.JobsItems.Find(model.Id);
+    //     jobsFromDb.Vacancy = model.Vacancy;
+    //     jobsFromDb.Requirements = model.Requirements;
+    //     jobsFromDb.Responsibilities = model.Responsibilities;
+    //     jobsFromDb.PicSrc = model.PicSrc;
+    //     
+    //     _context.JobsItems.Update(jobsFromDb);
+    //     _context.SaveChanges();
+    //     
+    //     return RedirectToAction("Index");
+    // }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
