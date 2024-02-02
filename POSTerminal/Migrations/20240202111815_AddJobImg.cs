b5b797bbca228fa41ebcb433ba849cf8e10c2545
Mikhail Shashin <shashinma@icloud.com>
2024-02-02 14:31:03 +0300

    Add ImageId property to JobsItem model
    The ImageId property has been added to the JobsItem model and all database and model modifications have been implemented. This data-linking will help in maintaining the relationship between a JobItem and its associated image. A new migration named 'AddJobImg' has been created to handle these changes.

---
+++ POSTerminal/Migrations/20240202111815_AddJobImg.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace POSTerminal.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddJobImg : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ImageId",
+                table: "JobsItems",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobsItems_ImageId",
+                table: "JobsItems",
+                column: "ImageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_JobsItems_ImageItems_ImageId",
+                table: "JobsItems",
+                column: "ImageId",
+                principalTable: "ImageItems",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_JobsItems_ImageItems_ImageId",
+                table: "JobsItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_JobsItems_ImageId",
+                table: "JobsItems");
+
+            migrationBuilder.DropColumn(
+                name: "ImageId",
+                table: "JobsItems");
+        }
+    }
+}
